7ca7175a07be981c5c1dd6ca173d317fc56d3123
Haeherfeder <gitDeDemos@haeherfeder.de>
2019-01-16 21:00:14 +0100

    Delete JARFileFilter.java

--- TEst/src/de/haeherfeder/DeDeGaPlEngine/JARFileFilter.java
+++
@@ -1,9 +0,0 @@
-package de.haeherfeder.DeDeGaPlEngine;
-import java.io.File;
-import java.io.FileFilter;
-
-public class JARFileFilter implements FileFilter {
-	public boolean accept(File f){
-		return f.getName().toLowerCase().endsWith(".jar");
-  }
-}
